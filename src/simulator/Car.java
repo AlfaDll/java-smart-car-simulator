16e3b72943ed8e86ac31cb4cb71d325c47f63678
66970 <66970@etu.he2b.be>
2026-02-20 17:20:51 +0100

    feat: add Car class

---
+++ src/simulator/Car.java
@@ -0,0 +1,12 @@
+package simulator;
+
+public class Car {
+    // Position on the road (in meters)
+    private double position;
+
+    // Current speed (in meters per second)
+    private double speed;
+
+    // Current acceleration (negative value means braking)
+    private double acceleration;
+}
