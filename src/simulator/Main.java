5a7106ea9a54a4eef8d74094cef3606fe3816b51
66970 <66970@etu.he2b.be>
2026-02-21 15:30:20 +0100

    feat: setting up the window

---
+++ src/simulator/Main.java
@@ -0,0 +1,10 @@
+package simulator;
+
+public class Main {
+    public static void main() {
+        // Making the window
+        WindowSimulator myWindow = new WindowSimulator();
+        // Literally setting visible
+        myWindow.setVisible(true);
+    }
+}
---
+++ src/simulator/WindowSimulator.java
@@ -0,0 +1,19 @@
+package simulator;
+import javax.swing.*;
+
+/* "extends" allows the class to do everything
+that the parent class (JFrame here) can do. It's
+called Inheritance.*/
+public class WindowSimulator extends JFrame {
+
+    // Constructor for  WindowSimulator
+    public WindowSimulator(){
+        setTitle("Smart Car Simulator");
+
+        // Means kill the program and free memory when red cross (top right) is clicked
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Initial size of the window
+        setSize(800,600);
+    }
+}
